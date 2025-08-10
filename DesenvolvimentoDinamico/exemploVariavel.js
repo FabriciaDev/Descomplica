2c36b370ac06c980a74e883ffaa334c60f0b542e
FabriciaDev <phabymandalas@gmail.com>
2025-08-09 22:01:33 -0300

    segundo commit

--- DesenvolvimentoDinamico/exemploVariavel.js
+++ DesenvolvimentoDinamico/exemploVariavel.js
@@ -1,21 +1,67 @@
-//exemplo de uso de var. let e const em JavaScript
-const externo = "Olá eu sou uma variavel global"
-//Decklaração de variável usando var
+// Exemplo de uso de var, let e const em JavaScript
+
+const externo = "Olá, eu sou uma constante global!";
+
+// Declaração de uma variável usando var
 function exemploVar() {
     if (true) {
         var mensagem = "Olá, Faculdade Descomplica! Eu sou uma var...";
-    }
-}
-//Chamando a função exemploVar
-exemploVar();
-//Exemplo de erro e correção
-var mensagem = "Olá, Faculdade Descomplica! Eu sou uma var...";
-console.log(var mensagem = "mensagem");
-//Declaração de uma variavel usanddo let
-function exemploLet*() {
-    if true {
-        let mensagem = "Essa mensagem só vale aqui dentro porque é uma let"
         console.log(mensagem);
-        
     }
-} 
+    
+}
+
+// Chamando a função exemploVar
+exemploVar();
+
+// Exemplo de erro e correção
+var mensagem1 = "Olá, Faculdade Descomplica! Escopo externo!";
+console.log(mensagem1);
+
+// Declaração de uma variável usando let
+function exemploLet() {
+    if (true) {
+        let mensagem = "Olá, Faculdade Descomplica! Eu sou uma let...";
+        console.log(mensagem);
+    }
+    //console.log(mensagem);
+}
+
+// Chamando a função exemploLet
+exemploLet();
+
+// Exemplo de erro e correção
+let mensagem = "Olá, Descomplica! Let externo!";
+console.log(mensagem);
+
+// Declaração de uma constante usando const
+function exemploConstante() {
+    const mensagem = "Olá, Faculdade Descomplica! Eu sou uma constante...";
+    console.log(mensagem);
+}
+
+// Chamando a função exemploConst
+exemploConstante();
+
+// Exemplo externo de const
+console.log(externo);
+
+// Exemplo externo tentar alterar constante global
+// externo = "Teste";
+console.log(externo);
+
+// Demonstrando o escopo global de var
+if (true) {
+    var escopoGlobal = "Sou uma var global!";
+}
+
+console.log(escopoGlobal);
+
+// Demonstrando o escopo de bloco de let
+if (true) {
+    let escopoBloco = "Sou um bloco!";
+    console.log(escopoBloco);
+}
+
+// Exemplo do escopo inacessível
+// console.log(escopoBloco); // Isso causaria um erro
