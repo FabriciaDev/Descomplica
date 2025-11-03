22f068bb07ae8d1d9b07a5bfa9c83c9d864f701f
FabriciaDev <phabymandalas@gmail.com>
2025-11-03 13:58:12 -0300

    aula tipos primitivos

---
+++ CriacoesAplicacoesSistemas/TiposPrimitivos/TiposPrimitivos/src/App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hello, World!");
+
+
+// 8 bits, intervevalo de -128 a 127
+byte oitoBits = 100;
+// 16 bits, intervalo de -32.768 a 32.767
+short numeroMuitoPequeno = 32;
+// 32 bits, intervalo de -2^31 a 2^31-1
+int Inteiro = 32000;
+// 64 bits, intervalo de -2^63 a 2^63-1
+long numeroMuitoGrande = 164000;
+// 32 bits, ponto flutuante de precisão simples
+float decimalSimples = 31;
+// 64 bits ponto flutuante de precisão dupla
+double decimalPreciso = 34.5;
+// 16 bits, caractere Unicode
+char caractere = 'A';
+// true ou false
+boolean verdadeiroFalso = true;
+
+System.out.println(oitoBits);
+System.out.println(numeroMuitoPequeno);
+System.out.println(Inteiro);
+System.out.println(numeroMuitoGrande);
+System.out.println(decimalSimples);
+System.out.println(decimalPreciso);
+System.out.println(caractere);
+System.out.println(verdadeiroFalso);
+}
+}
